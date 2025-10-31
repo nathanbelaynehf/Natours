4e28b4bd8d2ec3b770358e17421ad5cc2a08df9c
nathanbelaynehf <nathanbelayneh.1f@gmail.com>
2025-10-31 07:35:47 -0500

    3rd commit

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(cors({
     'http://127.0.0.1:5173',     // Vite default port
     'http://localhost:5174',     // Alternative Vite port
     'http://127.0.0.1:5174',
-    'https://natours-1-zy39.onrender.com/',
-    'https://front-natours-cdek.vercel.app/' 
+    'https://natours-1-zy39.onrender.com',
+    'https://front-natours-cdek.vercel.app' 
          // Alternative Vite port
   ],
   credentials: true,
